fff49235184e734b46f8df904fd03a535f83c1d4
mnabeelkhan1985 <mnabeelkhan1985@gmail.com>
2021-04-14 14:41:46 +0500

    JS Assignment No.04

---
+++ app.js
@@ -0,0 +1,11 @@
+var value1="variable1",value2="variable2",value3="variable3";
+document.write(value1,value2,value3);
+var a=" Balochistan",$=" Sindh",_pro=" Punjab",CamelCase=" KhyberPukhtukhwa",Alert1=" GilgitBaltistan";
+document.write(a,$,_pro,CamelCase,Alert1)
+document.write("<h1>Rules for Naming JS Variables</h1>")
+var num1="numbers",$="$",pak_="_",letters="letters";
+document.write("<p>Variable Names can only contain numbers,$,_ and letters for Example<span><b>$ my_First Variable</b></span><p>",num1,$,pak_,letters)
+document.write("<p>Variable must begin with letter,$ or_ for Example<span><b>$name,_name and name</b></span><p>")
+document.write("<p>Variable names are case<span><b>  sensitive</b></span><p>")
+document.write("<p>Variable names should not be JS<span><b>  keyword</b></span><p>")
+// var 1="pakistan", a block="turkey",-value="saudi arabia" alert="palestine";
